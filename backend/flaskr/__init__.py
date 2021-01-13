25ae03776f427b67fc9dae48e15e720ae99cc61c
Mohannad Alahmadi <moha.1111@gmail.com>
2021-01-13 20:37:50 +0000

    feat: Added pagination and GET endpoints
    - defined a function to handle pagination.
    - added CORS and set to allow *
    - added @app.route('/categories') endpoint to GET all questions categories from db
    - added @app.route('/questions') endpoint to GET all questions from db

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,17 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+# utility for paginating questions
+def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)# default page to 1
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -16,7 +27,17 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  #DONE
+  CORS(app, resources={'/': {'origins': '*'}})
 
+  @app.after_request
+  def after_request(response):
+      '''
+      Sets access control.
+      '''
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+      return response
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
@@ -26,21 +47,66 @@ def create_app(test_config=None):
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  #DONE
+  @app.route('/categories')
+  def get_categories():
 
+    # first it gets all categories and add them to list
+    categories = Category.query.all()
+    categories_dict = {}
+    for category in categories:
+        categories_dict[category.id] = category.type
+
+    # if no categories found, then abort with 404
+    if (len(categories_dict) == 0):
+        abort(404)
+
+    # Otherwise, return requested Data
+    return jsonify({
+        'success': True,
+        'categories': categories_dict
+    })
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
+  number of total questions, current category, categories.
+   
 
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  #DONE
+  @app.route('/questions')
+  def get_questions():
+      
 
+    # query all questions then paginate them
+    selection = Question.query.all()
+    total_questions = len(selection)
+    current_questions = paginate_questions(request, selection)
+
+    # first it gets all categories and add them to dictionary
+    categories = Category.query.all()
+    categories_dict = {}
+    for category in categories:
+        categories_dict[category.id] = category.type
+
+    # if no categories found, then abort with 404
+    if (len(current_questions) == 0):
+        abort(404)
+
+    # Otherwise, return requested Data
+    return jsonify({
+        'success': True,
+        'questions': current_questions,
+        'total_questions': total_questions,
+        'categories': categories_dict
+    })
   '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
